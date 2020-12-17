530ab5f20ca324e8525bf2f452f95d9cacd1b624
Alexidis <Alexidis911@gmail.com>
2020-12-17 14:33:57 +0300

    4 задача без умножения

--- lesson1.py
+++ lesson1.py
@@ -102,6 +102,25 @@ def fourth_paragraph3():
         rank *= 10
         
     print(f'Наибольшая цифра в веденом числе {greatest_num}')
+    
+def fourth_paragraph4():
+    """Поиск наибольшей цифры в числе."""
+    custom_int = int(input('Введите целое положительное число '))
+
+    # заносим делитель в переменную
+    divider = 10
+    # получаем значение едениц
+    greatest_num = custom_int % divider
+
+    # проверяем все разряды
+    while custom_int >= divider:
+        # сокращаем введенное число на делитель
+        custom_int //= divider
+        # находим "еденицу" текущего ранга
+        curr_num = custom_int % divider
+        greatest_num = max(curr_num, greatest_num)
+
+    print(f'Наибольшая цифра в веденом числе {greatest_num}')
 
 def fifth_paragraph():
     """Сложная экономическая задача"""
@@ -150,6 +169,7 @@ if __name__ == '__main__':
     fourth_paragraph()
     fourth_paragraph2()
     fourth_paragraph3()
+    fourth_paragraph4()
     fifth_paragraph()
     print(sixth_paragraph())
 
