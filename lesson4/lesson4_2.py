65c89b95c0eba36ce13cead989a64fcc130ec0c0
Alexidis <Alexidis911@gmail.com>
2020-12-28 08:37:31 +0300

    Переделал на итерирование по индексам списка

--- lesson4/lesson4_2.py
+++ lesson4/lesson4_2.py
@@ -1,5 +1,7 @@
 def greatest_prev(sub_list):
     """Фильтрация значений относительно предыдущих"""
+    # значения индексов списка
+    list_indices = range(1, len(sub_list))
     # генерируем новый список в котором остались только элементы со значением большим чем у соседа слева
-    new_list = [i for i in sub_list if sub_list.index(i) > 0 and float(i) > float(sub_list[sub_list.index(i)-1])]
+    new_list = [sub_list[i] for i in list_indices if float(sub_list[i]) > float(sub_list[i - 1])]
     return new_list
