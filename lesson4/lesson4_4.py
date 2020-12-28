db1c2250198b13b5f173e302e5e8691f469e75b1
Alexidis <Alexidis911@gmail.com>
2020-12-28 08:47:22 +0300

    Реализация через collections.Counter

--- lesson4/lesson4_4.py
+++ lesson4/lesson4_4.py
@@ -1,3 +1,8 @@
+from collections import Counter
+
+
 def unique_list(some_list):
     """Список без повторений"""
-    return [i for i in some_list if some_list.count(i) == 1]
+    # создаем словарь с расчитанныым количеством вхождений каждого элемента введеного списка
+    list_counter = Counter(some_list)
+    return [item for item in some_list if list_counter[item] == 1]
