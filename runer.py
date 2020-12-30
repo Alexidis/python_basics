ce8cc4d27a27de1cbfb0ee8d4f8e18bfa4b9f589
Alexidis911 <Alexidis911@gmail.com>
2020-12-30 10:03:34 +0300

    Сделал единую точку входа для всех ДЗ

---
+++ runer.py
@@ -0,0 +1,43 @@
+import lesson1 as l1
+import lesson2 as l2
+import lesson3 as l3
+import lesson4 as l4
+
+if __name__ == '__main__':
+    # первая практика
+    l1.p1_print_simple()
+    l1.p2_sec_to_time()
+    l1.p3_wild_s()
+    l1.p4_greatest_digit_1()
+    l1.p4_greatest_digit_2()
+    l1.p4_greatest_digit_3()
+    l1.p4_greatest_digit_4()
+    l1.p5_some_economics()
+    print(l1.p6_fitness_tracker())
+
+    # вторая практика
+    l2.p1_print_variables()
+    l2.p2_elem_replacement()
+    l2.p3_show_season_list()
+    l2.p3_show_season_dict()
+    l2.p4_separate_string()
+    l2.p5_get_ratings()
+    l2.p6_pivot_table()
+
+    # третья практика
+    l3.p1_div_func()
+    l3.p2_user_form_printer()
+    l3.p3_greatest_sum()
+    l3.p4_my_pow1()
+    l3.p4_my_pow2()
+    l3.p5_sum_quit()
+    l3.p6_int_func()
+
+    # четвертая практика
+    l4.p1_calc_salary()
+    l4.p2_greatest_prev()
+    l4.p3_twenty_multiples()
+    l4.p4_unique_list()
+    l4.p5_list_production()
+    l4.p6_iter_creator()
+    l4.p7_inner_fact()
